63eefa982a9a144010f61a7d2cd45bc618589633
Hayden Eubanks <72290782+haydenubanx@users.noreply.github.com>
2025-07-31 00:24:29 +0100

    feat: added in error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,20 +9,40 @@ def emotion_detector(text_to_analyse):
 
     request_json = { "raw_document": { "text": text_to_analyse } }
 
-    response = requests.post(url, headers=headers, json=request_json)
-
-
-    data = json.loads(response.text)
-    emotions = data['emotionPredictions'][0]['emotion']
-
-    response_json = {
-        'anger': emotions['anger'],
-        'disgust': emotions['disgust'],
-        'fear': emotions['fear'],
-        'joy': emotions['joy'],
-        'sadness': emotions['sadness'],
-        'dominant_emotion': max(emotions, key=emotions.get)
+    default_response_json = {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
     }
 
 
-    return response_json
+    if not text_to_analyse:
+        status_code = 400
+
+    else:
+        response = requests.post(url, headers=headers, json=request_json)
+        status_code = response.status_code
+
+        data = json.loads(response.text)
+        emotions = data['emotionPredictions'][0]['emotion']
+
+        response_json = {
+            'anger': emotions['anger'],
+            'disgust': emotions['disgust'],
+            'fear': emotions['fear'],
+            'joy': emotions['joy'],
+            'sadness': emotions['sadness'],
+            'dominant_emotion': max(emotions, key=emotions.get)
+        }
+
+    # Make sure status code is within 200 range i.e. 2xx (Succesful responses)
+    if status_code == 400:
+        return default_response_json, 400
+    elif status_code // 100 != 2:
+        return {'ERROR' : 'RESPONSE WAS NOT SUCCESSFUL'}, status_code
+
+
+    return response_json, 200
--- server.py
+++ server.py
@@ -7,13 +7,19 @@ app = Flask(__name__)
 def detect_emotion():
     input_json = request.get_json()
     statement = input_json.get('text', '')
-    result = emotion_detector(statement)
-    response = (
-        f"For the given statement, the system response is "
-        f"'anger': {result['anger']}, 'disgust': {result['disgust']}, "
-        f"'fear': {result['fear']}, 'joy': {result['joy']} and "
-        f"'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
-    )
+    result, status_code = emotion_detector(statement)
+
+    if status_code == 200:
+        response = jsonify(
+            f"For the given statement, the system response is "
+            f"'anger': {result['anger']}, 'disgust': {result['disgust']}, "
+            f"'fear': {result['fear']}, 'joy': {result['joy']} and "
+            f"'sadness': {result['sadness']}. The dominant emotion is {result['dominant_emotion']}."
+        ), 200
+    elif status_code == 400 or result['dominant_emotion'] == None:
+        response =  jsonify({"message": "Invalid text! Please try again."}), 400
+    else:
+        response = jsonify({"message": "INTERNAL SERVER ERROR"}), 500
     return response
 
 
